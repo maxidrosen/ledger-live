f2d8fb38c81f49f9101824ff375da898b1ca56ae
mateuszpalosz-ext <mateusz.palosz-ext@ledger.fr>
2026-04-29 15:24:46 +0200

    feat: added translations

--- apps/ledger-live-desktop/src/renderer/families/aleo/modals/send/steps/utils.ts
+++ apps/ledger-live-desktop/src/renderer/families/aleo/modals/send/steps/utils.ts
@@ -1,19 +1,20 @@
 import type { AleoAccount } from "@ledgerhq/live-common/families/aleo/types";
 import type { AccountLike } from "@ledgerhq/types-live";
+import type { TFunction } from "i18next";
 
 export const isAleoAccount = (acc: AccountLike): acc is AleoAccount => "aleoResources" in acc;
 
-// Helper function to get estimated signing time based on the number of records being signed.
 export const SIGNING_RECORDS_TIME = 12500;
 
-export const getEstimatedSigningTime = (recordCount: number): string => {
+// Helper function to get estimated signing time based on the number of records being signed.
+export const getEstimatedSigningTime = (recordCount: number, t: TFunction): string => {
   const totalSeconds = (recordCount * SIGNING_RECORDS_TIME) / 1000;
 
   if (totalSeconds < 60) {
-    return `~${Math.round(totalSeconds)}s`;
+    return `~${Math.round(totalSeconds)} ${t("common.second_short")}`;
   }
 
   const flooredSeconds = Math.floor(totalSeconds / 30) * 30;
   const minutes = flooredSeconds / 60;
-  return `~${minutes} min`;
+  return `~${minutes} ${t("common.minute_short")}`;
 };
